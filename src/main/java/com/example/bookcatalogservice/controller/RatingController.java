56dabc6036d4955dbeb043c3e93fb4288a1ca2f0
tihomir.valchanov <tihomir.valchanov@accedia.com>
2023-02-20 10:20:55 +0200

    Refactor code

--- src/main/java/com/example/bookcatalogservice/controller/RatingController.java
+++ src/main/java/com/example/bookcatalogservice/controller/RatingController.java
@@ -3,6 +3,7 @@ package com.example.bookcatalogservice.controller;
 import com.example.bookcatalogservice.model.Rating;
 import com.example.bookcatalogservice.service.RatingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,11 @@ public class RatingController {
     private final RatingService ratingService;
 
     @GetMapping("/{bookId}")
-    public Mono<Rating> getRatingByBookId(@PathVariable("bookId") Long id) {
-        return ratingService.findByBookId(id);
+    public Mono<ResponseEntity<Rating>> getRatingByBookId(@PathVariable("bookId") Long id) {
+        return ratingService.findByBookId(id)
+                            .map(ResponseEntity::ok)
+                            .defaultIfEmpty(ResponseEntity.notFound()
+                                                          .build());
     }
 
     @GetMapping
